68ef2103edbd218ba53ca581d61881cfae71227b
Alisher <alisher0201010@gmail.com>
2021-05-08 01:05:45 +0600

    search

--- client/src/components/LinksList.js
+++ client/src/components/LinksList.js
@@ -1,36 +1,49 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+const { useState } = React;
 
 export const LinksList = ({ links }) => {
-  if (!links.length) {
-    return <p className="center">Ссылок пока нет</p>
-  }
 
-  return (
-    <table>
-      <thead>
-      <tr>
-        <th>№</th>
-        <th>Оригинальная</th>
-        <th>Сокращенная</th>
-        <th>Открыть</th>
-      </tr>
-      </thead>
+    const [search, setValue] = useState("");
+    const handleChange = (e) => setValue(e.target.value);
 
-      <tbody>
-      { links.map((link, index) => {
-        return (
-          <tr key={link._id}>
-            <td>{index + 1}</td>
-            <td>{link.from}</td>
-            <td>{link.to}</td>
-            <td>
-              <Link to={`/detail/${link._id}`}>Открыть</Link>
-            </td>
-          </tr>
-        )
-      }) }
-      </tbody>
-    </table>
-  )
+    if (!links.length) {
+        return <p className="center">Ссылок пока нет</p>
+    }
+    return (
+        <div>
+            <input type="search" value={search} onChange={handleChange}/>
+            <table>
+                <thead>
+                <tr>
+                    <th>№</th>
+                    <th>Оригинальная</th>
+                    <th>Сокращенная</th>
+                    <th>Открыть</th>
+                </tr>
+                </thead>
+
+                <tbody>
+                { links.filter((value) => {
+                    if (value.from && value.to) {
+                        return value.from.toLowerCase().includes(search.toLowerCase())||
+                        value.to.toLowerCase().includes(search.toLowerCase());
+                    }
+                    return true;
+                }).map((link, index) => {
+                    return (
+                        <tr key={link._id}>
+                            <td>{index + 1}</td>
+                            <td>{link.from}</td>
+                            <td>{link.to}</td>
+                            <td>
+                                <Link to={`/detail/${link._id}`}>Открыть</Link>
+                            </td>
+                        </tr>
+                    )
+                }) }
+                </tbody>
+            </table>
+        </div>
+    )
 }
